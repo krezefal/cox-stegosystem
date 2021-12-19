6f42381dd6f053c9c011b07446b50e345ed7cb37
Timofey Klimov <klimovtimofeyy@gmail.com>
2021-12-19 21:11:01 +0300

    add functionality to work with image array, remove unnecessary funcs

--- stego/utils.go
+++ stego/utils.go
@@ -1,12 +1,17 @@
-package pkg
+package stego
 
 import (
 	"golang.org/x/image/bmp"
 	"image"
+	"image/color"
 	"math/rand"
 	"os"
 	"time"
 )
+
+const RED = "RED"
+const GREEN = "GREEN"
+const BLUE = "BLUE"
 
 func ReadImage(filePath string) (image.Image, error) {
 	imgFile, err := os.Open(filePath)
@@ -29,14 +34,62 @@ func WriteImage(filePath string, img image.Image) error {
 	return nil
 }
 
-func ConvertToRGBA(img image.Image) *image.RGBA {
+func getComponent(img image.Image, component string) [][]byte {
 
 	bounds := img.Bounds()
-	imgRGBA := image.NewRGBA(image.Rectangle{Min: image.Point{}, Max: image.Point{X: bounds.Dx(), Y: bounds.Dy()}})
+	width, height := bounds.Max.X, bounds.Max.Y
 
-	for i := 0; i < bounds.Dx(); i++ {
-		for j := 0; j < bounds.Dy(); j++ {
-			imgRGBA.Set(i, j, img.At(i, j))
+	singleComponentArray := make([][]byte, height)
+	for i := range singleComponentArray {
+		singleComponentArray[i] = make([]byte, width)
+	}
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			switch component {
+			case RED:
+				singleComponentArray[y][x] = uint8(r)
+			case GREEN:
+				singleComponentArray[y][x] = uint8(g)
+			case BLUE:
+				singleComponentArray[y][x] = uint8(b)
+			}
+		}
+	}
+
+	return singleComponentArray
+}
+
+func setComponent(img image.Image, singleComponentArray [][]byte, component string) *image.RGBA {
+
+	bounds := img.Bounds()
+	width, height := bounds.Max.X, bounds.Max.Y
+	imgRGBA := image.NewRGBA(image.Rectangle{Min: image.Point{}, Max: image.Point{X: width, Y: height}})
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+
+			pix := img.At(x, y)
+			r, g, b, a := pix.RGBA()
+
+			newPix := color.RGBA{
+				R: uint8(r),
+				G: uint8(g),
+				B: uint8(b),
+				A: uint8(a),
+			}
+
+			switch component {
+			case RED:
+				newPix.R = singleComponentArray[y][x]
+			case GREEN:
+				newPix.G = singleComponentArray[y][x]
+			case BLUE:
+				newPix.B = singleComponentArray[y][x]
+			}
+
+			imgRGBA.Set(x, y, newPix)
 		}
 	}
 
